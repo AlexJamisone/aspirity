a2327e89bfc49a511b5ba74762ee971cfa99280b
Alex Jamison <business.homeit@gmail.com>
2024-06-26 19:37:53 +0300

    finsih Avatar component

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,12 +1,49 @@
-import Image from "next/image";
+import { cn } from '@/utils';
+import { cva, VariantProps } from 'class-variance-authority';
+import Image from 'next/image';
+import { forwardRef, HTMLAttributes } from 'react';
 
-export default function Avatar() {
-    return (
-        <div className="cursor-pointer relative">
-            <Image src='/avatar.png' width={160} height={160} alt="avatar" className="" />
-            <div className="flex items-center opacity-0 transition rounded-full hover:opacity-100 bg-black/50 absolute size-full top-0 justify-center">
-               <Image src='/camera.svg' priority width={24} height={24} alt="camera"/> 
-            </div>
-        </div>
-    )
-}
+const avatarVariant = cva('relative cursor-pointer', {
+	variants: {
+		size: {
+			lg: 'size-40',
+			md: 'size-24',
+			sm: 'size-24',
+			xs: 'size-6',
+		},
+	},
+	defaultVariants: {
+		size: 'lg',
+	},
+});
+
+interface AvatarProps
+	extends HTMLAttributes<HTMLDivElement>,
+		VariantProps<typeof avatarVariant> {}
+
+const Avatar = forwardRef<HTMLDivElement, AvatarProps>(
+	({ size, className, ...props }, ref) => {
+		return (
+			<div
+				className={cn(avatarVariant({ size, className }))}
+				ref={ref}
+				{...props}
+			>
+				<Image src="/avatar.png" fill alt="avatar" />
+				<div className="absolute top-0 flex size-full items-center justify-center rounded-full bg-black/50 opacity-0 transition hover:opacity-100">
+					<div
+						className={
+							size === 'xs'
+								? 'relative size-4'
+								: 'relative size-[24px]'
+						}
+					>
+						<Image src="/camera.svg" fill alt="camera" />
+					</div>
+				</div>
+			</div>
+		);
+	},
+);
+
+export default Avatar;
